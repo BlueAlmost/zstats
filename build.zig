4b37ba39fe8ef09f5b5e6fe11761f4a98baee640
BlueAlmost <git.blue.almost@gmail.com>
2023-02-12 16:05:02 +0100

    aligned with new build process

--- build.zig
+++ build.zig
@@ -1,65 +1,20 @@
 const std = @import("std");
-const Builder = std.build.Builder;
-const Pkg = std.build.Pkg;
-
-const pkgs = struct {
-    const zstats = Pkg{
-        .name = "zstats",
-        .source = .{ .path = "./zstats.zig" },
-        .dependencies = &[_]Pkg{ },
-    };
-};
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe_targets = [_]Target{
-        // .{ .name = "foo", .src = "foo.zig", .desc ="foo"},
-    };
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{.path = "./zstats.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
 
-    for (exe_targets) |e_target| {
-        e_target.build(b);
-    }
+    unit_tests.addAnonymousModule("zstats", .{
+        .source_file = .{.path = "./zstats.zig"},
+    });
 
-    const exe_tests = b.addTest("./zstats.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    exe_tests.addPackage(pkgs.zstats);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    const unit_tests_step = b.step("test", "Run unit tests");
+    unit_tests_step.dependOn(&unit_tests.step);
 }
 
-//---------------------------------------------------------------------------
-
-const Target = struct {
-    name: []const u8,
-    src: []const u8,
-    desc: []const u8,
-
-    pub fn build(self: Target, b: *std.build.Builder) void {
-
-        // write executables here instead
-        b.exe_dir = b.pathFromRoot("./bin");
-
-        // const target = b.standardTargetOptions(.{});
-        const mode = b.standardReleaseOptions();
-
-        var exe = b.addExecutable(self.name, self.src);
-        exe.setBuildMode(b.standardReleaseOptions());
-        // exe.setTarget(target);
-        exe.setBuildMode(mode);
-
-        exe.addPackage(pkgs.zstats);
-
-        exe.install();
-
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-    }
-};
